f4884a71eeb7cd5fe6b5d8b23773938239f74bdd
Junninho Thomas <jthomas2016@bklawtech.com>
2016-05-04 21:16:41 +0000

    multiple cities

--- weather_api-master/index.js
+++ weather_api-master/index.js
@@ -1,29 +1,35 @@
-window.onload = function() {
-	var formElement = document.getElementById('form');
-	var inputElement = document.getElementById('input')
-	console.log("onload")
-
-	formElement.onsubmit = function(event) {
+$(function() { 
+	$('form').on('submit', function (event) {
+		$('.output').empty();
 		event.preventDefault();
-		var cityName = inputElement.value;
-		getWeather(cityName);
-	};
-};
+		var userInput = $('input').val();
+		var cities = userInput.split(",");
+		
+		for (var i=0; i < cities.length; i++) {
+			var cityName = cities[i];
+			getWeather(cityName);
+		}
+		
+	});
+});
 
-function printWeather(event) {
-	var response = event.target.responseText;
-	document.write(response);
+function printWeather(response) {
+	var element =	$('<p>', {
+		text: response.main.temp
+	});
+	var city =	$('<p>', {
+		text: response.name
+	});
+	$('.output').append(city);
+	$('.output').append(element);
 }
 
-function getWeather(cityName) {
-  var url = 'http://api.openweathermap.org/data/2.5/weather' + 
+function getWeather(cities) {
+  var url = 'https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather' + 
   '?appid=c2f2d170f6f6fc336058e9851edb828c'+
-  '&q=' + cityName +
-  '&units=inperial';
+  '&q=' + cities +
+  '&units=imperial';
 
 
-  var xhrObject = new XMLHttpRequest();
-  xhrObject.open('GET', url, true);
-  xhrObject.onload = printWeather;
-  xhrObject.send();
+	$.get(url, printWeather)
 }
